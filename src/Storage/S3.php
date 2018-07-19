04cbbb99682ba53d989bbcc8b5e1546f56631abc
Vishnu B <vishnu.nxg@gmail.com>
2018-07-19 15:43:32 +0530

    Changes the destroy method to align with aws v3

--- src/Storage/S3.php
+++ src/Storage/S3.php
@@ -72,8 +72,12 @@ class S3 implements StorageInterface
      */
     public function remove(array $filePaths)
     {
-        if ($filePaths) {
-            $this->s3Client->deleteObjects(['Bucket' => $this->attachedFile->s3_object_config['Bucket'], 'Objects' => $this->getKeys($filePaths)]);
+         if ($filePaths) {
+            $this->s3Client->deleteObjects(['Bucket' => $this->attachedFile->s3_object_config['Bucket'],
+                'Delete' => [
+                    'Objects' => $this->getKeys($filePaths)
+                ]
+             ]);
         }
     }
 
